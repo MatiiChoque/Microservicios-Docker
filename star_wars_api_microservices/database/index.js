6e861520192d6f6039fda5ee88abc41ff616b513
MatiiChoque <choquematia@gmail.com>
2023-09-25 20:51:41 -0300

    cargando datos a repositorio propio

---
+++ star_wars_api_microservices/database/index.js
@@ -0,0 +1,5 @@
+const server = require("./src/server");
+
+server.listen(8004, () => {
+  console.log("Database is running on PORT 8004");
+});
---
+++ star_wars_api_microservices/database/src/middlewares/validateModel.js
@@ -0,0 +1,8 @@
+module.exports = (req, res, next) => {
+  const { model } = req.params;
+  if (["Character", "Film", "Planet"].includes(model)) {
+    next();
+  } else {
+    throw Error("Invalid Model");
+  }
+};
