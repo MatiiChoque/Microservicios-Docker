1262fe547dbccb0cfc4025529a5a88764267d40f
MatiiChoque <choquematia@gmail.com>
2023-09-26 19:48:31 -0300

    [add] axios request

--- star_wars_api_microservices/characters/src/data/index.js
+++ star_wars_api_microservices/characters/src/data/index.js
@@ -1,8 +1,9 @@
-const characters = require("./characters.json");
+const axios = require("axios");
 
 module.exports = {
   list: async () => {
-    return characters;
+    const results = await axios.get("http://database:8004/Character");
+    return results.data;
   },
   create: async () => {
     throw Error("Hay un error en la BDD creando un personaje");
--- star_wars_api_microservices/films/src/data/index.js
+++ star_wars_api_microservices/films/src/data/index.js
@@ -1,8 +1,9 @@
-const films = require("./films.json");
+const axios = require("axios");
 
 module.exports = {
   list: async () => {
-    return films;
+    const results = await axios.get("http://database:8004/Film");
+    return results.data;
   },
   create: async () => {
     throw Error("Hay un error en la BDD creando un film");
