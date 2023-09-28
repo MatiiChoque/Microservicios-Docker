6600eb448bca4766380637412e63ded726e17bea
MatiiChoque <choquematia@gmail.com>
2023-09-28 17:50:01 -0300

    [upd] Character - CRUD - index

--- star_wars_api_microservices/characters/src/controllers/index.js
+++ star_wars_api_microservices/characters/src/controllers/index.js
@@ -1,6 +1,9 @@
 const { catchedAsync } = require("../utils");
 
 module.exports = {
+  getCharacter: catchedAsync(require("./getCharacter")),
   getCharacters: catchedAsync(require("./getCharacters")),
   createCharacter: catchedAsync(require("./createCharacter")),
+  updateCharacter: catchedAsync(require("./updateCharacter")),
+  deleteCharacter: catchedAsync(require("./deleteCharacter")),
 };
