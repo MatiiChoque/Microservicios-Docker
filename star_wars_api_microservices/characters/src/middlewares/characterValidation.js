79fc2836ffc6a81a2fde571a929bf4bbc318c877
MatiiChoque <choquematia@gmail.com>
2023-09-28 17:57:35 -0300

    [upd] Validation - good practices

--- star_wars_api_microservices/characters/src/middlewares/characterValidation.js
+++ star_wars_api_microservices/characters/src/middlewares/characterValidation.js
@@ -1,7 +1,55 @@
 const { ClientError } = require("../utils/errors");
 
 module.exports = (req, res, next) => {
-  const { name } = req.body;
-  if (name) return next();
-  else throw new ClientError("Error en el nombre", 401);
+  const {
+    name,
+    height,
+    mass,
+    hair_color,
+    skin_color,
+    eye_color,
+    birth_year,
+    gender,
+    homeworld,
+  } = req.body;
+  if (
+    !name ||
+    !height ||
+    !mass ||
+    !hair_color ||
+    !skin_color ||
+    !eye_color ||
+    !birth_year ||
+    !gender ||
+    !homeworld
+  ) {
+    throw new ClientError("All fields are required", 400);
+  }
+
+  if (
+    !homeworld ||
+    typeof homeworld !== "object" ||
+    !homeworld._id ||
+    !homeworld.name
+  ) {
+    throw new ClientError(
+      "Homeworld must be an object with _id and name properties",
+      400
+    );
+  }
+
+  const { films } = req.body;
+  if (!films || !Array.isArray(films) || films.length === 0) {
+    throw new ClientError("Films must be a non-empty array", 400);
+  }
+
+  for (const film of films) {
+    if (!film || typeof film !== "object" || !film._id || !film.title) {
+      throw new ClientError(
+        "Each film in the films array must be an object with _id and title properties",
+        400
+      );
+    }
+  }
+  return next();
 };
--- star_wars_api_microservices/films/src/middlewares/filmValidation.js
+++ star_wars_api_microservices/films/src/middlewares/filmValidation.js
@@ -1,7 +1,46 @@
 const { ClientError } = require("../utils/errors");
 
 module.exports = (req, res, next) => {
-  const { title } = req.body;
-  if (title) return next();
-  else throw new ClientError("Error en el titulo", 401);
+  const {
+    title,
+    opening_crawl,
+    director,
+    producer,
+    release_date,
+    characters,
+    planets,
+  } = req.body;
+  if (
+    !title ||
+    !opening_crawl ||
+    !director ||
+    !producer ||
+    !release_date ||
+    !characters ||
+    !planets
+  ) {
+    throw new ClientError("Missing required fields", 400);
+  }
+  if (typeof title !== "string") {
+    throw new ClientError("Title must be a string", 400);
+  }
+  if (typeof opening_crawl !== "string") {
+    throw new ClientError("Opening crawl must be a string", 400);
+  }
+  if (typeof director !== "string") {
+    throw new ClientError("Director must be a string", 400);
+  }
+  if (typeof producer !== "string") {
+    throw new ClientError("Producer must be a string", 400);
+  }
+  if (typeof release_date !== "string") {
+    throw new ClientError("Release date must be a string", 400);
+  }
+  if (!Array.isArray(characters)) {
+    throw new ClientError("Characters must be an array", 400);
+  }
+  if (!Array.isArray(planets)) {
+    throw new ClientError("Planets must be an array", 400);
+  }
+  return next();
 };
